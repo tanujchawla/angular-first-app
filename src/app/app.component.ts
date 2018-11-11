c0506dc09395bc8c2a9541a0be143345a5a0f4cf
Tanuj Chawla <chawla.tanuj96@gmail.com>
2018-11-11 22:14:35 +0530

    #first commit till video 30

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,13 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  /*
+  styles: [`
+  h3 {
+    color : dodgerblue;
+  }
+  `]  */
 })
 export class AppComponent {
-  title = 'my-first-app';
+  //title = 'my-first-app';
 }
